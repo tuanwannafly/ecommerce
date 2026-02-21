e575ee18c5cb12985a1c23447aac56be9f7ccb2c
testbest <tunawillgetit1030@gmail.com>
2026-02-21 19:55:52 +0700

    Pagination is hardcoded and starts on page 1 (skips first page).

--- src/main/java/com/tuan/ecommerce/controller/UserController.java
+++ src/main/java/com/tuan/ecommerce/controller/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,8 +40,9 @@ public class UserController {
         this.userService = userService;
     }
     @GetMapping
-    public ResponseEntity<Page<UserResponse>> getAllUsers() {
-        Page<UserResponse> userResponses  = userService.getAllUser(1, 10);
+    public ResponseEntity<Page<UserResponse>> getAllUsers(@RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        Page<UserResponse> userResponses  = userService.getAllUser(page, size);
         return  ResponseEntity.ok(userResponses);
     }
     @GetMapping("/{id}")
